8c9829c581e1c2680f60990695d840fb5ca3ecaf
Abdullah <abdullahboy241@gmail.com>
2024-03-19 05:51:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Abdullah, would you like to learn some TypeScript today?”
+var personname = "Abdullah";
+console.log("Hello ".concat(personname, ", would you like to learn some TypeScript today?"));
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var myName = "Abdullah"; // This saves the name
+console.log(myName.toLowerCase()); // Shows the name in all small letters
+console.log(myName.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
+// Shows the name with the First Letter Big
---
+++ main.ts
@@ -0,0 +1,15 @@
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Abdullah, would you like to learn some TypeScript today?”
+
+let personname :string = "Abdullah";
+console.log(`Hello ${personname}, would you like to learn some TypeScript today?`); 
+
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Abdullah, would you like to learn some TypeScript today?”
+
+let myName = "Abdullah"; // This saves the name
+console.log(myName.toLowerCase()); // Shows the name in all small letters
+console.log(myName.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase()); 
+// Shows the name with the First Letter Big
